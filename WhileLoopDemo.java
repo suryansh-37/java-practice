f1d7b0159145aa76eb57723f12c423d44084a62a
suryansh-37 <suryanshsahu37@gmail.com>
2025-06-02 17:41:39 +0530

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,10 @@
+public class WhileLoopDemo {
+    public static void main(String args[]){
+        int count = 1;
+        while (count <= 5){
+            System.out.println("Counting: " +count);
+            count++;
+        }
+    }
+    
+}
