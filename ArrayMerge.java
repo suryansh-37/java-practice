79446536ec0cfa7fca207b6f834c6d9643327bd2
suryansh-37 <suryanshsahu37@gmail.com>
2025-06-08 23:54:28 +0530

    Create ArrayMerge.java

---
+++ ArrayMerge.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArrayMerge {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+
+        int[] merged = new int[arr1.length + arr2.length];
+
+        System.arraycopy(arr1, 0, merged, 0, arr1.length);
+        System.arraycopy(arr2, 0, merged, arr1.length, arr2.length);
+
+        System.out.println("Merged Array:");
+
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
